2398bb19a59a364158e3bf439ccdb78a19150420
Azalea Gui <me@hydev.org>
2023-01-19 16:16:50 -0500

    [M] Move fonts

--- run.sh
+++ run.sh
@@ -66,4 +66,4 @@ PYTHONPATH=. python infer.py --model_dir=experiments_finetune/checkpoint/experim
 PYTHONPATH=. python infer_by_text.py --model_dir=experiments_finetune/checkpoint/experiment_0 \
                 --batch_size=32 \
                 --embedding_id=67 \
-                --save_dir=save_dir/                --save_dir=save_dir/
+                --save_dir=save_dir/
